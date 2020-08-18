d5888ac1b9965a4eb8f3d2a0f5a6c317cf382710
Nicole <nicoleduu@gmail.com>
2020-08-18 19:34:36 -0400

    addedGoogleMaps

--- client/components/EventsFeed.jsx
+++ client/components/EventsFeed.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Event from './Event.jsx';
 
-export default function EventsFeed(props) {
-  let events = [];
-  //creates events for each event in feed
-  if (props.events && Object.keys(props.events).length > 0) {
-    events = props.events.map((event, index) => {
-      return <Event
-        {...event}
-        userUpdate={props.userUpdate}
-        key={`EventsFeed${index}`}
-      />
-    })
-  }
-  return (
-    <div className="events">
-      {events}
-    </div>
-  );
+export default function EventsFeed (props) {
+	let events = [];
+	//creates events for each event in feed
+	if (props.events && Object.keys(props.events).length > 0) {
+		events = props.events.map((event, index) => {
+			return <Event
+				{...event}
+				userUpdate={props.userUpdate}
+				key={`EventsFeed${index}`}
+			/>
+		})
+	}
+	return (
+		<div className="events">
+			{events}
+		</div>
+	);
 }
+
+//handleupdate or handlelete event which gets passed down as a prop, update event feed and send fetch request to the backend(delete this event), 
