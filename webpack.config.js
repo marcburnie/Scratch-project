1fc18ad8f48b15710d5378f0f0e14997e69afebb
Marc Burnie <marcburnie@hotmail.com>
2020-08-20 15:43:53 -0500

    merged with master on org

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-<<<<<<< HEAD
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devtool: 'eval-source-map',
   mode: process.env.NODE_ENV,
@@ -42,18 +43,14 @@ module.exports = {
           },
         ],
       },
-    ]
+    ],
   },
   devServer: {
     port: 8080,
     // contentBase: path.resolve(__dirname, '/dist'),
     // publicPath: '/dist/',
     proxy: {
-      '/api/**': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-      '/assets/**': {
+      '/': {
         target: 'http://localhost:3000',
         secure: false,
       },
@@ -64,75 +61,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './client/index.html',
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new Dotenv(),
   ],
-=======
-const Dotenv = require('dotenv-webpack');
-
-
-module.exports = {
-	entry: './client/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	devtool: 'eval-source-map',
-	mode: process.env.NODE_ENV,
-	module: {
-		rules: [
-			{
-				test: /.(css|scss)$/,
-				// include: [path.resolve(__dirname, '/node_modules/react-datepicker/'), path.resolve(__dirname, '/node_modules/bootstrap/')],
-				// exclude: /node_modules/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /.jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-					},
-				},
-			},
-			{
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				use: [
-					{
-						// loads files as base64 encoded data url if image file is less than set limit
-						loader: 'url-loader',
-						options: {
-							// if file is greater than the limit (bytes), file-loader is used as fallback
-							limit: 8192,
-						},
-					},
-				],
-			},
-		],
-	},
-	devServer: {
-		port: 8080,
-		// contentBase: path.resolve(__dirname, '/dist'),
-		// publicPath: '/dist/',
-		proxy: {
-			'/': {
-				target: 'http://localhost:3000',
-				secure: false,
-			},
-		},
-		hot: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './client/index.html',
-		}),
-		new MiniCssExtractPlugin(),
-		new Dotenv(),
-	],
-	node: {
-		fs: 'empty',
-		http2: 'empty',
-	},
->>>>>>> e761a6ff71da52d37ec554fde6660324e8835373
+  node: {
+    fs: 'empty',
+    http2: 'empty',
+  },
 };
